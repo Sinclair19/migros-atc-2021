084678017e8cc08918e11969479f84b50c26f598
Sinclair19 <zsq0219@outlook.com>
2025-01-26 15:36:45 +0800

    update client.sh and moving.sh

--- client.sh
+++ client.sh
@@ -8,4 +8,4 @@ echo 16 > /proc/sys/net/rdma_rxe/last_mrn
 docker run --rm --name cont-static --network rdma-net --ip 10.0.1.6 \
    --security-opt seccomp:unconfined --ulimit memlock=1073741824 \
    --cap-add=ALL --memory=1g --kernel-memory=1G --device /dev/infiniband/ \
-   migros/perftest:latest ib_send_bw -d rxe0 -n 100000 10.0.1.7 -b
+   docker-repo/perftest:latest ib_send_bw -d rocep2s4 -n 100000 10.0.1.7 -b
--- moving.sh
+++ moving.sh
@@ -2,21 +2,21 @@
 
 docker rm cont-moving
 
-ssh welt@192.168.172.128 'echo "123456" | sudo -S docker checkpoint create cont-moving ckpt1'
+ssh root@192.168.172.130 docker checkpoint create cont-moving ckpt1
 
 docker create --name cont-moving --network rdma-net --ip 10.0.1.7 \
    --security-opt seccomp:unconfined --ulimit memlock=1073741824 \
    --cap-add=ALL --memory=1g --kernel-memory=1G --device /dev/infiniband/ \
-   migros/perftest:latest ib_send_bw -d rxe0 -n 100000
+   docker-repo/perftest:latest ib_send_bw -d rocep2s4 -n 100000
 
-SRC_ID=$(ssh welt@192.168.172.128 'echo "123456" | sudo -S docker inspect --format="{{.Id}}" cont-moving')
+SRC_ID=$(ssh root@192.168.172.130 docker inspect --format="{{.Id}}" cont-moving)
 DEST_ID=$(docker inspect --format="{{.Id}}" cont-moving)
-CONTAINERS=/var/lib/docker/containers/
+CONTAINERS=/var/lib/docker/containers
 
 echo 64 > /proc/sys/net/rdma_rxe/last_qpn 
 echo 64 > /proc/sys/net/rdma_rxe/last_mrn
-scp -r welt@192.168.172.128:$CONTAINERS/$SRC_ID/checkpoints/ckpt1 $CONTAINERS/$DEST_ID/checkpoints/
+scp -r root@192.168.172.130:$CONTAINERS/$SRC_ID/checkpoints/ckpt1 $CONTAINERS/$DEST_ID/checkpoints/
 
-ssh welt@192.168.172.128 'echo "123456" | sudo -S docker rm cont-moving'
+ssh root@192.168.172.130 docker rm cont-moving
 
 docker start cont-moving --checkpoint ckpt1
